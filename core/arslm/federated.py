5533d29075c460b60c3149bdd4a8758082d3cc86
benjaminpolydeq <kbenjio9@gmail.com>
2025-12-20 10:35:27 +0000

    Create federated.py
    Gestion de l’apprentissage fédéré.
    Coordination multi-clients
    Agrégation sécurisée des gradients
    Aucun transfert de données brutes

---
+++ core/arslm/federated.py
@@ -0,0 +1,7 @@
+class FederatedCoordinator:
+    """
+    Federated learning orchestration (secure aggregation placeholder)
+    """
+
+    def aggregate(self, client_updates):
+        return sum(client_updates) / len(client_updates)
